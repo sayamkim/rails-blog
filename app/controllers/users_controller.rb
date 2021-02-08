4f03ccf5b7e6097858bcf54e9501a78ef7465f20
Sayam Kim <sayamkim@gmail.com>
2021-02-08 17:25:29 -0600

    Added authentication helpers to controllers and modified UI access to functionality.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 2)
   end
 
@@ -13,12 +14,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
-
     if @user.update(user_params)
       flash[:notice] = "Profile updated succesfully"
       redirect_to @user
@@ -31,6 +29,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to the blog!"
       redirect_to articles_path
     else
@@ -41,5 +40,9 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
