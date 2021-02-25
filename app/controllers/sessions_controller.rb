5b4ef5b714becb37201e5edb4fb04bd6144a26d2
Sayam Kim <sayamkim@gmail.com>
2021-02-24 23:00:13 -0600

    Shut down Intercom on logout.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,6 +18,7 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     flash[:notice] = "Logged out"
+    IntercomRails::ShutdownHelper::intercom_shutdown_helper(cookies)
     redirect_to root_path
   end
 end
